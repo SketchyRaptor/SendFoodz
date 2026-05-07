ea8357fca23d1485dd32d1cf7ff861165bb6acc1
SketchyRaptor <69042398+SketchyRaptor@users.noreply.github.com>
2026-05-07 13:38:57 +0800

    Update MerchantDashboard.Designer.cs with Edit Menu and Sales History buttons

--- MerchantDashBoard.Designer.cs
+++ MerchantDashBoard.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace LogIn1
+namespace LogIn1
 {
     partial class MerchantDashBoard
     {
@@ -31,39 +31,41 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.panelEditMenu = new System.Windows.Forms.Panel();
+            this.btnSalesHistory = new System.Windows.Forms.Button();
             this.btnEditMenu = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel3.SuspendLayout();
-            this.panel4.SuspendLayout();
+            this.panelEditMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.panelContent);
             this.panel1.Controls.Add(this.panel2);
-            this.panel1.Location = new System.Drawing.Point(58, 44);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1161, 612);
+            this.panel1.Size = new System.Drawing.Size(1259, 681);
             this.panel1.TabIndex = 0;
             // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.btnLogout);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Location = new System.Drawing.Point(-1, -1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1161, 77);
+            this.panel2.Size = new System.Drawing.Size(1257, 80);
             this.panel2.TabIndex = 0;
             // 
             // label1
@@ -76,14 +78,18 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "SendFoodz";
             // 
-            // button1
+            // btnLogout
             // 
-            this.button1.Location = new System.Drawing.Point(991, 22);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(124, 32);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "Logout";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnLogout.BackColor = System.Drawing.Color.Red;
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.ForeColor = System.Drawing.Color.White;
+            this.btnLogout.Location = new System.Drawing.Point(1099, 22);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(124, 32);
+            this.btnLogout.TabIndex = 2;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            this.btnLogout.Click += new System.EventHandler(this.btnLogout_Click);
             // 
             // pictureBox1
             // 
@@ -95,36 +101,51 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // panel3
+            // panelContent
             // 
-            this.panel3.BackColor = System.Drawing.Color.White;
-            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.panel4);
-            this.panel3.Location = new System.Drawing.Point(577, 200);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(407, 288);
-            this.panel3.TabIndex = 1;
+            this.panelContent.BackColor = System.Drawing.Color.White;
+            this.panelContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelContent.Controls.Add(this.panelEditMenu);
+            this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContent.Location = new System.Drawing.Point(0, 80);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(1257, 599);
+            this.panelContent.TabIndex = 1;
             // 
-            // panel4
+            // panelEditMenu
             // 
-            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel4.Controls.Add(this.btnEditMenu);
-            this.panel4.Location = new System.Drawing.Point(41, 33);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(327, 212);
-            this.panel4.TabIndex = 0;
+            this.panelEditMenu.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelEditMenu.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelEditMenu.Controls.Add(this.btnSalesHistory);
+            this.panelEditMenu.Controls.Add(this.btnEditMenu);
+            this.panelEditMenu.Location = new System.Drawing.Point(280, 150);
+            this.panelEditMenu.Name = "panelEditMenu";
+            this.panelEditMenu.Size = new System.Drawing.Size(700, 300);
+            this.panelEditMenu.TabIndex = 0;
             // 
             // btnEditMenu
             // 
-            this.btnEditMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnEditMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(238)))), ((int)(((byte)(144)))));
             this.btnEditMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditMenu.Location = new System.Drawing.Point(47, 66);
+            this.btnEditMenu.Location = new System.Drawing.Point(50, 50);
             this.btnEditMenu.Name = "btnEditMenu";
-            this.btnEditMenu.Size = new System.Drawing.Size(239, 63);
+            this.btnEditMenu.Size = new System.Drawing.Size(280, 80);
             this.btnEditMenu.TabIndex = 0;
-            this.btnEditMenu.Text = "Edit your Menu";
+            this.btnEditMenu.Text = "📋 Edit Menu";
             this.btnEditMenu.UseVisualStyleBackColor = false;
+            this.btnEditMenu.Click += new System.EventHandler(this.btnEditMenu_Click);
+            // 
+            // btnSalesHistory
+            // 
+            this.btnSalesHistory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(216)))), ((int)(((byte)(230)))));
+            this.btnSalesHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalesHistory.Location = new System.Drawing.Point(370, 50);
+            this.btnSalesHistory.Name = "btnSalesHistory";
+            this.btnSalesHistory.Size = new System.Drawing.Size(280, 80);
+            this.btnSalesHistory.TabIndex = 1;
+            this.btnSalesHistory.Text = "📊 Sales History";
+            this.btnSalesHistory.UseVisualStyleBackColor = false;
+            this.btnSalesHistory.Click += new System.EventHandler(this.btnSalesHistory_Click);
             // 
             // MerchantDashBoard
             // 
@@ -133,13 +154,12 @@
             this.ClientSize = new System.Drawing.Size(1259, 681);
             this.Controls.Add(this.panel1);
             this.Name = "MerchantDashBoard";
-            this.Text = "MerchantDashBoard";
+            this.Text = "Merchant Dashboard";
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel3.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
+            this.panelEditMenu.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -149,10 +169,11 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnLogout;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Panel panelEditMenu;
         private System.Windows.Forms.Button btnEditMenu;
+        private System.Windows.Forms.Button btnSalesHistory;
     }
 }
