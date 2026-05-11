7b4750f7d41e8ba497c84de7b0bc9cea61f5fead
Yaesha <63915@LAPTOP-UV90TENK>
2026-05-11 16:13:27 +0800

    ADDED View ORder

---
+++ OrderStorage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogIn1
+{
+    public static class OrderStorage
+    {
+        public static List<Order> AllOrders { get; set; } = new List<Order>();
+    }
+
+    public class Order
+    {
+        private static int nextId = 1;
+        public int OrderId { get; private set; }
+        public string CustomerName { get; set; }
+        public string MerchantName { get; set; }
+        public List<string> ItemsSummary { get; set; } = new List<string>();
+        public decimal Total { get; set; }
+        public string Status { get; set; }
+        public string Rider { get; set; }
+        public string Stage { get; set; }
+        public List<string> HistoryLog { get; set; } = new List<string>();
+
+        public Order()
+        {
+            OrderId = nextId++;
+        }
+    }
+}
---
+++ ViewOrder.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace LogIn1
+{
+    partial class ViewOrder
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dgvOrders = new System.Windows.Forms.DataGridView();
+            this.colOrderId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colMerchant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colItems = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colStatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colStage = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colRider = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblCustomerName = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.lblOrderHistory = new System.Windows.Forms.Label();
+            this.lstHistory = new System.Windows.Forms.ListBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).BeginInit();
+            this.panelTop.SuspendLayout();
+            this.SuspendLayout();
+
+            // dgvOrders
+            this.dgvOrders.AllowUserToAddRows = false;
+            this.dgvOrders.AllowUserToDeleteRows = false;
+            this.dgvOrders.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvOrders.BackgroundColor = System.Drawing.Color.White;
+            this.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOrders.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+                this.colOrderId,
+                this.colMerchant,
+                this.colItems,
+                this.colTotal,
+                this.colStatus,
+                this.colStage,
+                this.colRider});
+            this.dgvOrders.Location = new System.Drawing.Point(12, 80);
+            this.dgvOrders.Name = "dgvOrders";
+            this.dgvOrders.ReadOnly = true;
+            this.dgvOrders.RowHeadersVisible = false;
+            this.dgvOrders.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvOrders.Size = new System.Drawing.Size(960, 300);
+            this.dgvOrders.TabIndex = 0;
+            this.dgvOrders.SelectionChanged += new System.EventHandler(this.dgvOrders_SelectionChanged);
+
+            // Columns
+            this.colOrderId.HeaderText = "Order ID";
+            this.colOrderId.Name = "colOrderId";
+            this.colOrderId.ReadOnly = true;
+
+            this.colMerchant.HeaderText = "Merchant";
+            this.colMerchant.Name = "colMerchant";
+            this.colMerchant.ReadOnly = true;
+
+            this.colItems.HeaderText = "Items";
+            this.colItems.Name = "colItems";
+            this.colItems.ReadOnly = true;
+
+            this.colTotal.HeaderText = "Total (₱)";
+            this.colTotal.Name = "colTotal";
+            this.colTotal.ReadOnly = true;
+
+            this.colStatus.HeaderText = "Status";
+            this.colStatus.Name = "colStatus";
+            this.colStatus.ReadOnly = true;
+
+            this.colStage.HeaderText = "Current Stage";
+            this.colStage.Name = "colStage";
+            this.colStage.ReadOnly = true;
+
+            this.colRider.HeaderText = "Rider";
+            this.colRider.Name = "colRider";
+            this.colRider.ReadOnly = true;
+
+            // panelTop
+            this.panelTop.BackColor = System.Drawing.Color.FromArgb(255, 128, 0);
+            this.panelTop.Controls.Add(this.btnBack);
+            this.panelTop.Controls.Add(this.btnRefresh);
+            this.panelTop.Controls.Add(this.lblCustomerName);
+            this.panelTop.Controls.Add(this.lblTitle);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(1000, 70);
+            this.panelTop.TabIndex = 1;
+
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(20, 15);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(186, 37);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "My Orders";
+
+            this.lblCustomerName.AutoSize = true;
+            this.lblCustomerName.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.lblCustomerName.ForeColor = System.Drawing.Color.White;
+            this.lblCustomerName.Location = new System.Drawing.Point(220, 25);
+            this.lblCustomerName.Name = "lblCustomerName";
+            this.lblCustomerName.Size = new System.Drawing.Size(0, 23);
+
+            this.btnRefresh.BackColor = System.Drawing.Color.White;
+            this.btnRefresh.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRefresh.Location = new System.Drawing.Point(800, 18);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(100, 35);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = false;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+
+            this.btnBack.BackColor = System.Drawing.Color.LightGray;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Location = new System.Drawing.Point(680, 18);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(100, 35);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+
+            // Order History
+            this.lblOrderHistory.AutoSize = true;
+            this.lblOrderHistory.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.lblOrderHistory.Location = new System.Drawing.Point(12, 400);
+            this.lblOrderHistory.Name = "lblOrderHistory";
+            this.lblOrderHistory.Size = new System.Drawing.Size(112, 23);
+            this.lblOrderHistory.TabIndex = 2;
+            this.lblOrderHistory.Text = "Order History";
+
+            this.lstHistory.FormattingEnabled = true;
+            this.lstHistory.ItemHeight = 20;
+            this.lstHistory.Location = new System.Drawing.Point(12, 430);
+            this.lstHistory.Name = "lstHistory";
+            this.lstHistory.Size = new System.Drawing.Size(960, 144);
+            this.lstHistory.TabIndex = 3;
+
+            // ViewOrder form
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.lstHistory);
+            this.Controls.Add(this.lblOrderHistory);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.dgvOrders);
+            this.Name = "ViewOrder";
+            this.Text = "My Orders - Customer";
+            this.Load += new System.EventHandler(this.ViewOrder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOrders)).EndInit();
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.DataGridView dgvOrders;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblCustomerName;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblOrderHistory;
+        private System.Windows.Forms.ListBox lstHistory;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colOrderId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colMerchant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colItems;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colStatus;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colStage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colRider;
+    }
+}
