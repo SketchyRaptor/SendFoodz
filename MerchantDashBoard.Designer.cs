0b60f44505896ec142b03d2495730180bd9b2743
plazaanthonyb-bit <plazaanthonyb@gmail.com>
2026-05-13 18:57:28 +0800

    MerchantDashboardUpdated

--- MerchantDashBoard.Designer.cs
+++ MerchantDashBoard.Designer.cs
@@ -34,16 +34,16 @@ namespace LogIn1
             this.btnSalesHistory = new System.Windows.Forms.Button();
             this.btnEditMenu = new System.Windows.Forms.Button();
             this.btnUploadBackground = new System.Windows.Forms.Button();
-            this.picBackgroundPreview = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnLogout = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.picBackgroundPreview = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panelContent.SuspendLayout();
             this.panelEditMenu.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.picBackgroundPreview)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackgroundPreview)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,19 +55,21 @@ namespace LogIn1
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1259, 681);
+            this.panel1.Size = new System.Drawing.Size(1119, 545);
             this.panel1.TabIndex = 0;
             // 
             // panelContent
             // 
-            this.panelContent.BackColor = System.Drawing.Color.White;
+            this.panelContent.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.panelContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panelContent.Controls.Add(this.panelEditMenu);
             this.panelContent.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelContent.Location = new System.Drawing.Point(0, 80);
+            this.panelContent.Location = new System.Drawing.Point(0, 76);
+            this.panelContent.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelContent.Name = "panelContent";
-            this.panelContent.Size = new System.Drawing.Size(1257, 599);
+            this.panelContent.Size = new System.Drawing.Size(1117, 467);
             this.panelContent.TabIndex = 1;
             // 
             // panelEditMenu
@@ -78,18 +80,20 @@ namespace LogIn1
             this.panelEditMenu.Controls.Add(this.btnEditMenu);
             this.panelEditMenu.Controls.Add(this.btnUploadBackground);
             this.panelEditMenu.Controls.Add(this.picBackgroundPreview);
-            this.panelEditMenu.Location = new System.Drawing.Point(10, 14);
+            this.panelEditMenu.Location = new System.Drawing.Point(33, 33);
+            this.panelEditMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelEditMenu.Name = "panelEditMenu";
-            this.panelEditMenu.Size = new System.Drawing.Size(1242, 573);
+            this.panelEditMenu.Size = new System.Drawing.Size(1044, 406);
             this.panelEditMenu.TabIndex = 0;
             // 
             // btnSalesHistory
             // 
             this.btnSalesHistory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(173)))), ((int)(((byte)(216)))), ((int)(((byte)(230)))));
             this.btnSalesHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalesHistory.Location = new System.Drawing.Point(370, 50);
+            this.btnSalesHistory.Location = new System.Drawing.Point(227, 107);
+            this.btnSalesHistory.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSalesHistory.Name = "btnSalesHistory";
-            this.btnSalesHistory.Size = new System.Drawing.Size(280, 80);
+            this.btnSalesHistory.Size = new System.Drawing.Size(249, 64);
             this.btnSalesHistory.TabIndex = 1;
             this.btnSalesHistory.Text = "📊 Sales History";
             this.btnSalesHistory.UseVisualStyleBackColor = false;
@@ -99,9 +103,10 @@ namespace LogIn1
             // 
             this.btnEditMenu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(144)))), ((int)(((byte)(238)))), ((int)(((byte)(144)))));
             this.btnEditMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditMenu.Location = new System.Drawing.Point(50, 50);
+            this.btnEditMenu.Location = new System.Drawing.Point(227, 258);
+            this.btnEditMenu.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnEditMenu.Name = "btnEditMenu";
-            this.btnEditMenu.Size = new System.Drawing.Size(280, 80);
+            this.btnEditMenu.Size = new System.Drawing.Size(249, 64);
             this.btnEditMenu.TabIndex = 0;
             this.btnEditMenu.Text = "📋 Edit Menu";
             this.btnEditMenu.UseVisualStyleBackColor = false;
@@ -111,35 +116,27 @@ namespace LogIn1
             // 
             this.btnUploadBackground.BackColor = System.Drawing.Color.LightGray;
             this.btnUploadBackground.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.btnUploadBackground.Location = new System.Drawing.Point(50, 160);
+            this.btnUploadBackground.Location = new System.Drawing.Point(628, 294);
+            this.btnUploadBackground.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnUploadBackground.Name = "btnUploadBackground";
-            this.btnUploadBackground.Size = new System.Drawing.Size(200, 35);
+            this.btnUploadBackground.Size = new System.Drawing.Size(178, 28);
             this.btnUploadBackground.TabIndex = 2;
             this.btnUploadBackground.Text = "Upload Background Image";
             this.btnUploadBackground.UseVisualStyleBackColor = false;
             this.btnUploadBackground.Click += new System.EventHandler(this.btnUploadBackground_Click);
             // 
-            // picBackgroundPreview
-            // 
-            this.picBackgroundPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.picBackgroundPreview.Location = new System.Drawing.Point(300, 140);
-            this.picBackgroundPreview.Name = "picBackgroundPreview";
-            this.picBackgroundPreview.Size = new System.Drawing.Size(150, 80);
-            this.picBackgroundPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.picBackgroundPreview.TabIndex = 3;
-            this.picBackgroundPreview.TabStop = false;
-            // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel2.Controls.Add(this.btnLogout);
             this.panel2.Controls.Add(this.label1);
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1257, 80);
+            this.panel2.Size = new System.Drawing.Size(1117, 76);
             this.panel2.TabIndex = 0;
             // 
             // btnLogout
@@ -147,9 +144,10 @@ namespace LogIn1
             this.btnLogout.BackColor = System.Drawing.Color.Red;
             this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogout.ForeColor = System.Drawing.Color.White;
-            this.btnLogout.Location = new System.Drawing.Point(1099, 22);
+            this.btnLogout.Location = new System.Drawing.Point(982, 14);
+            this.btnLogout.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogout.Name = "btnLogout";
-            this.btnLogout.Size = new System.Drawing.Size(124, 32);
+            this.btnLogout.Size = new System.Drawing.Size(110, 39);
             this.btnLogout.TabIndex = 2;
             this.btnLogout.Text = "Logout";
             this.btnLogout.UseVisualStyleBackColor = false;
@@ -159,36 +157,50 @@ namespace LogIn1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(149, 17);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(132, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 37);
+            this.label1.Size = new System.Drawing.Size(162, 31);
             this.label1.TabIndex = 1;
             this.label1.Text = "SendFoodz";
+            // 
+            // picBackgroundPreview
+            // 
+            this.picBackgroundPreview.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picBackgroundPreview.Location = new System.Drawing.Point(589, 107);
+            this.picBackgroundPreview.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.picBackgroundPreview.Name = "picBackgroundPreview";
+            this.picBackgroundPreview.Size = new System.Drawing.Size(250, 155);
+            this.picBackgroundPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBackgroundPreview.TabIndex = 3;
+            this.picBackgroundPreview.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::LogIn1.Properties.Resources.ChatGPT_Image_Apr_17__2026__02_55_18_PM;
-            this.pictureBox1.Location = new System.Drawing.Point(17, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(15, 8);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(117, 69);
+            this.pictureBox1.Size = new System.Drawing.Size(104, 55);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // MerchantDashBoard
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1259, 681);
+            this.ClientSize = new System.Drawing.Size(1119, 545);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "MerchantDashBoard";
             this.Text = "Merchant Dashboard";
             this.panel1.ResumeLayout(false);
             this.panelContent.ResumeLayout(false);
             this.panelEditMenu.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.picBackgroundPreview)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBackgroundPreview)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
