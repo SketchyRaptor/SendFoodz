00d427e5a69b69e02a5aed7da7f66e386671425a
LIFFORD LAWAS <LIFFORD LAWAS@LAPTOP-V1PER2CM>
2026-05-15 20:25:25 +0800

    enhance Login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -24,8 +24,18 @@
             this.panel1 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBoxScroll = new System.Windows.Forms.PictureBox();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScroll)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitle
@@ -168,10 +178,104 @@
             this.label1.TabIndex = 9;
             this.label1.Text = "Fast and Easy Food Delivery";
             // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btnRight);
+            this.panel2.Controls.Add(this.btnLeft);
+            this.panel2.Controls.Add(this.pictureBoxScroll);
+            this.panel2.Location = new System.Drawing.Point(-8, -6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(585, 721);
+            this.panel2.TabIndex = 9;
+            // 
+            // pictureBoxScroll
+            // 
+            this.pictureBoxScroll.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxScroll.Location = new System.Drawing.Point(127, 306);
+            this.pictureBoxScroll.Name = "pictureBoxScroll";
+            this.pictureBoxScroll.Size = new System.Drawing.Size(330, 214);
+            this.pictureBoxScroll.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxScroll.TabIndex = 0;
+            this.pictureBoxScroll.TabStop = false;
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeft.ForeColor = System.Drawing.Color.Red;
+            this.btnLeft.Location = new System.Drawing.Point(65, 384);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(56, 54);
+            this.btnLeft.TabIndex = 1;
+            this.btnLeft.Text = "◀";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click_1);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.ForeColor = System.Drawing.Color.Red;
+            this.btnRight.Location = new System.Drawing.Point(463, 384);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(56, 54);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.Text = "▶";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(147, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(280, 40);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Welcome Back!";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(162, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(252, 40);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "SENDFOODZ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(46, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(489, 40);
+            this.label4.TabIndex = 5;
+            this.label4.Text = " Your Food Delivery Partner";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(73, 569);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(437, 40);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Fast. Fresh. Delivered.   ";
+            // 
             // Form1
             // 
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.ClientSize = new System.Drawing.Size(1344, 712);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "SendFoodz Login";
@@ -179,6 +283,9 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxScroll)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -195,5 +302,13 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.PictureBox pictureBoxScroll;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnRight;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,24 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LogIn1
 {
     public partial class Form1 : Form
     {
+        private List<string> imageUrls = new List<string>();
+        private int currentIndex = 0;
+        private Timer autoScrollTimer;
+        private PictureBox pictureBoxNext; // For smooth transitions
+        private bool isLoading = false;
+
+
+
         public static List<Account> accounts = new List<Account>()
         {
             new Account { Username = "User123", Password = "123", Role = "Customer" }
@@ -21,10 +32,208 @@ namespace LogIn1
             this.WindowState = FormWindowState.Maximized;
 
             this.Resize += Form1_Resize;
-           CenterLoginPanel();  // initial centering
+           //CenterLoginPanel();  // initial centering
             this.MinimumSize = new Size(800, 600);  // prevent extreme shrinking
 
+            LoadPartnerLogos();
 
+            // Display first logo
+            DisplayCurrentLogo();
+        }
+
+        private void LoadPartnerLogos()
+        {
+            // Add your logo image paths here
+            imageUrls.Add("https://images.openai.com/static-rsc-4/MjhY6xaWAAT25bTNWhka8T-dOBtsvmEfUsDqHA85ZQHmcWSrsML9ZP6FLGDGsdTGi-WnxlGnq8j4Rj_dfmldo4XKHcKeJR2jWIUr30ZN21EjOmoPyTeIRtew3GmqooubMiKI7yzq04JCXGdsEbSfwgDKJ36e3kQHFSx_hMx0m_g?purpose=inline");
+            imageUrls.Add("https://images.openai.com/static-rsc-4/U97WIAUWTsUQIvtoRxdYzL_BE22Ie5Hx3pm6JmiwbfkngYtwaiU9LlIb-X0Cwy61dnk2iWocymoDF0SE8N8MfPOscwNjlBNIXqZna3A0cNZrmYZa6Qwtq4j7l62BmNn_VPNpe63idbzNlsibrFYHyj_Blf9LptDvmNpsw1A0o3Y?purpose=inline");
+            imageUrls.Add("https://images.openai.com/static-rsc-4/HTM2y27eX9rrRinQk1ZCOz8vlS9iE_7kv-wNRlM3QLZBmWOCAugXnakgQ_7svCh600B258S1j4hAu_CMP_XDoKQqTbFXc57Xq55BMbphaYXBUcGyBBGB2ODEbox-7XMGoaMPIuEUymV1d4Y4DhGnMb-d56t5U0v9ocJU8cvBijk?purpose=inline");
+            imageUrls.Add("https://images.openai.com/static-rsc-4/FAEdzr1_rxoF4v3FoB0LB-xl0lCmXxdXaT7Mr0Uxjxaue4EGRy6C2ifWnjWUNLdEmLOBqvNZFBva7DLCcMjPQ0gciYnHyNQoBD_ueno8PX_HeRqdxuc3v8-o3ycHC3KK4Cb3iwzsLqFXzG8ZRnJewf7XmA3LNGTgsp72bG5A8-w?purpose=inline");
+            imageUrls.Add("https://images.openai.com/static-rsc-4/Z-kEXURVaZpE4GWCH6TXGaHqkWGKJO_Qzy8JaSKLjvFL7nY2xaV5oXrBwfEbsSk2qaV9_Pl9SO4ARLAnlA0jBSge5po29jpPzUsMEqmg1q9rxvP8qSPrl3S9DBh0xkGqm4kfRK4ch1PQhfDYyOz6F5YdxwvTKv0oIzngU_TOOUw?purpose=inline");
+
+            SetupPictureBoxForSmoothTransition();
+
+            // Start with first image
+            PreloadImages();
+            DisplayCurrentLogo();
+            StartAutoScroll();
+        }
+
+        private void SetupPictureBoxForSmoothTransition()
+        {
+            // Enable double buffering for smooth rendering
+            pictureBoxScroll.DoubleBuffered(true);
+
+            // Set high quality image rendering
+            pictureBoxScroll.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxScroll.BackColor = Color.FromArgb(240, 242, 245);
+        }
+
+        private async Task FadeTransition(Image newImage)
+        {
+            // Smooth fade animation
+            for (int opacity = 0; opacity <= 100; opacity += 10)
+            {
+                pictureBoxScroll.Image = newImage;
+                pictureBoxScroll.Refresh();
+                await Task.Delay(5);
+            }
+        }
+
+        private async void PreloadImages()
+        {
+            // Preload next image for instant display
+            _ = Task.Run(async () =>
+            {
+                foreach (string url in imageUrls)
+                {
+                    try
+                    {
+                        using (HttpClient client = new HttpClient())
+                        {
+                            await client.GetByteArrayAsync(url);
+                        }
+                    }
+                    catch { }
+                }
+            });
+        }
+
+        private void StartAutoScroll()
+        {
+            autoScrollTimer = new Timer();
+            autoScrollTimer.Interval = 4000; // 4 seconds for smoother experience
+            autoScrollTimer.Tick += async (s, e) =>
+            {
+                autoScrollTimer.Stop();
+                await GoToNextImage();
+                autoScrollTimer.Start();
+            };
+            autoScrollTimer.Start();
+        }
+
+        private async Task GoToNextImage()
+        {
+            if (imageUrls.Count == 0) return;
+
+            currentIndex++;
+            if (currentIndex >= imageUrls.Count) currentIndex = 0;
+
+            await DisplayCurrentLogoSmooth();
+        }
+
+        private async Task GoToPreviousImage()
+        {
+            if (imageUrls.Count == 0) return;
+
+            currentIndex--;
+            if (currentIndex < 0) currentIndex = imageUrls.Count - 1;
+
+            await DisplayCurrentLogoSmooth();
+        }
+
+        private async Task DisplayCurrentLogoSmooth()
+        {
+            if (isLoading || imageUrls.Count == 0) return;
+
+            isLoading = true;
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    byte[] imageBytes = await client.GetByteArrayAsync(imageUrls[currentIndex]);
+                    using (MemoryStream ms = new MemoryStream(imageBytes))
+                    {
+                        Image newImage = Image.FromStream(ms);
+
+                        // Smooth cross-fade
+                        await CrossFadeImage(newImage);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                isLoading = false;
+            }
+        }
+
+        private async Task CrossFadeImage(Image newImage)
+        {
+            // Create temporary PictureBox for fade effect
+            PictureBox tempBox = new PictureBox
+            {
+                Image = newImage,
+                Size = pictureBoxScroll.Size,
+                Location = pictureBoxScroll.Location,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Visible = true
+            };
+
+            this.Controls.Add(tempBox);
+            tempBox.BringToFront();
+
+            // Fade in new image
+            for (int opacity = 0; opacity <= 100; opacity += 5)
+            {
+                tempBox.BackColor = Color.FromArgb(opacity * 255 / 100, 0, 0, 0);
+                await Task.Delay(3);
+            }
+
+            // Set the actual PictureBox image
+            pictureBoxScroll.Image = newImage;
+
+            // Fade out temp box
+            for (int opacity = 100; opacity >= 0; opacity -= 10)
+            {
+                tempBox.BackColor = Color.FromArgb(opacity * 255 / 100, 0, 0, 0);
+                await Task.Delay(2);
+            }
+
+            tempBox.Dispose();
+        }
+
+        private async void DisplayCurrentLogo()
+        {
+            if (imageUrls.Count > 0 && currentIndex >= 0 && currentIndex < imageUrls.Count)
+            {
+                // Show loading indicator
+                pictureBoxScroll.Image = null;
+                pictureBoxScroll.BackColor = Color.LightGray;
+
+                try
+                {
+                    using (HttpClient client = new HttpClient())
+                    {
+                        byte[] imageBytes = await client.GetByteArrayAsync(imageUrls[currentIndex]);
+                        using (MemoryStream ms = new MemoryStream(imageBytes))
+                        {
+                            pictureBoxScroll.Image = Image.FromStream(ms);
+                            pictureBoxScroll.SizeMode = PictureBoxSizeMode.StretchImage;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    pictureBoxScroll.BackColor = Color.Red;
+                    MessageBox.Show($"Error loading image: {ex.Message}");
+                }
+            }
+        }
+
+        // Button Click Events - Connect these to your btnLeft and btnRight
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            // Go to previous logo
+            currentIndex--;
+            if (currentIndex < 0)
+            {
+                //currentIndex = partnerLogos.Count - 1; // Loop to last
+            }
+            DisplayCurrentLogo();
         }
 
 
@@ -35,7 +244,7 @@ namespace LogIn1
             //CenterLoginPanel();
         }
 
-       
+       /*
         private void CenterLoginPanel()
         {
             if (panel1 == null) return;
@@ -43,7 +252,7 @@ namespace LogIn1
             int y = (this.ClientSize.Height - panel1.Height) / 2;
             panel1.Location = new Point(Math.Max(0, x), Math.Max(0, y));
         }
-       
+       */
         private void btnLogin_Click(object sender, EventArgs e)
         {
             string username = txtUsername.Text;
@@ -117,5 +326,28 @@ namespace LogIn1
         };
 
         private void Form1_Load(object sender, EventArgs e) { }
+
+        private async void btnLeft_Click_1(object sender, EventArgs e)
+        {
+            autoScrollTimer?.Stop();
+            await GoToPreviousImage();
+            autoScrollTimer?.Start();
+        }
+
+        private async void btnRight_Click(object sender, EventArgs e)
+        {
+            autoScrollTimer?.Stop();
+            await GoToNextImage();
+            autoScrollTimer?.Start();
+        }
+    }
+    public static class ControlExtensions
+    {
+        public static void DoubleBuffered(this Control control, bool enable)
+        {
+            var prop = typeof(Control).GetProperty("DoubleBuffered",
+                System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+            prop?.SetValue(control, enable, null);
+        }
     }
 }
